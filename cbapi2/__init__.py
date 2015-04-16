4eba030b7cc6b21517f2d77baaacb1bf0e3219d9
Jason Garman <jason.garman@kyrus-tech.com>
2015-04-16 13:13:24 -0400

    add CbProcess, CbSensor, CbBinary to public interface

--- cbapi2/__init__.py
+++ cbapi2/__init__.py
@@ -7,5 +7,6 @@ except Exception, e:
 
 from cbapi2 import MultiCbApi2, CbApi2, from_ui
 from cbapi2 import CbChildProcEvent, CbNetConnEvent, CbRegModEvent, CbFileModEvent, CbModLoadEvent
+from cbapi2 import CbProcess, CbSensor, CbBinary
 from cbapi2 import write_event_csv
 
